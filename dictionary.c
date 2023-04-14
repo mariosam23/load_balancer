c687dafce70794cafdd36c4a92e794bfea80591a
Mario <sampetrumario11@gmail.com>
2023-04-14 11:56:56 +0300

    just starting the job

---
+++ dictionary.c
@@ -0,0 +1,376 @@
+#include "dictionary.h"
+
+linked_list_t *ll_create(unsigned int data_size)
+{
+    linked_list_t* ll;
+
+    ll = malloc(sizeof(*ll));
+	DIE(!ll, "malloc failed");
+
+    ll->head = NULL;
+    ll->data_size = data_size;
+    ll->size = 0;
+
+    return ll;
+}
+
+void ll_add_nth_node(linked_list_t* list, unsigned int n, const void* new_data)
+{
+    ll_node_t *prev, *curr;
+    ll_node_t* new_node;
+
+    if (!list) {
+        return;
+    }
+
+    if (n > list->size) {
+        n = list->size;
+    }
+
+    curr = list->head;
+    prev = NULL;
+    while (n > 0) {
+        prev = curr;
+        curr = curr->next;
+        --n;
+    }
+
+    new_node = malloc(sizeof(*new_node));
+    new_node->data = malloc(list->data_size);
+    memcpy(new_node->data, new_data, list->data_size);
+
+    new_node->next = curr;
+    if (!prev) {
+        list->head = new_node;
+    } else {
+        prev->next = new_node;
+    }
+
+    list->size++;
+}
+
+ll_node_t *ll_remove_nth_node(linked_list_t* list, unsigned int n)
+{
+    ll_node_t *prev, *curr;
+
+    if (!list || !list->head) {
+        return NULL;
+    }
+
+    if (n > list->size - 1) {
+        n = list->size - 1;
+    }
+
+    curr = list->head;
+    prev = NULL;
+    while (n > 0) {
+        prev = curr;
+        curr = curr->next;
+        --n;
+    }
+
+    if (!prev) {
+        list->head = curr->next;
+    } else {
+        prev->next = curr->next;
+    }
+
+    list->size--;
+
+    return curr;
+}
+
+unsigned int ll_get_size(linked_list_t* list)
+{
+     if (!list) {
+        return -1;
+    }
+
+    return list->size;
+}
+
+void ll_free(linked_list_t** pp_list)
+{
+    ll_node_t* currNode;
+
+    if (!pp_list || !*pp_list) {
+        return;
+    }
+
+    while (ll_get_size(*pp_list) > 0) {
+        currNode = ll_remove_nth_node(*pp_list, 0);
+        free(currNode->data);
+        currNode->data = NULL;
+        free(currNode);
+        currNode = NULL;
+    }
+
+    free(*pp_list);
+    *pp_list = NULL;
+}
+
+void ll_print_int(linked_list_t* list)
+{
+    ll_node_t* curr;
+
+    if (!list) {
+        return;
+    }
+
+    curr = list->head;
+    while (curr != NULL) {
+        printf("%d ", *((int*)curr->data));
+        curr = curr->next;
+    }
+
+    printf("\n");
+}
+
+void ll_print_string(linked_list_t* list)
+{
+    ll_node_t* curr;
+
+    if (!list) {
+        return;
+    }
+
+    curr = list->head;
+    while (curr != NULL) {
+        printf("%s ", (char*)curr->data);
+        curr = curr->next;
+    }
+
+    printf("\n");
+}
+
+/*
+ * Functii de comparare a cheilor:
+ */
+int compare_function_ints(void *a, void *b)
+{
+	int int_a = *((int *)a);
+	int int_b = *((int *)b);
+
+	if (int_a == int_b) {
+		return 0;
+	} else if (int_a < int_b) {
+		return -1;
+	} else {
+		return 1;
+	}
+}
+
+int compare_function_strings(void *a, void *b)
+{
+	char *str_a = (char *)a;
+	char *str_b = (char *)b;
+
+	return strcmp(str_a, str_b);
+}
+
+/*
+ * Functii de hashing:
+ */
+unsigned int hash_function_int(void *a)
+{
+	/*
+	 * Credits: https://stackoverflow.com/a/12996028/7883884
+	 */
+	unsigned int uint_a = *((unsigned int *)a);
+
+	uint_a = ((uint_a >> 16u) ^ uint_a) * 0x45d9f3b;
+	uint_a = ((uint_a >> 16u) ^ uint_a) * 0x45d9f3b;
+	uint_a = (uint_a >> 16u) ^ uint_a;
+	return uint_a;
+}
+
+unsigned int hash_function_string(void *a)
+{
+	/*
+	 * Credits: http://www.cse.yorku.ca/~oz/hash.html
+	 */
+	unsigned char *puchar_a = (unsigned char*) a;
+	unsigned long hash = 5381;
+	int c;
+
+	while ((c = *puchar_a++))
+		hash = ((hash << 5u) + hash) + c; /* hash * 33 + c */
+
+	return hash;
+}
+
+void key_val_free_function(void *data)
+{
+    free(((info_t *)data)->key);
+    free(((info_t *)data)->value);
+}
+
+hashtable_t *ht_create(unsigned int hmax, unsigned int (*hash_function)(void*),
+		int (*compare_function)(void*, void*),
+		void (*key_val_free_function)(void*))
+{
+	hashtable_t *ht = malloc(sizeof(*ht));
+    DIE(!ht, "malloc failed");
+	
+	ht->hmax = hmax;
+
+	ht->buckets = malloc(sizeof(*ht->buckets) * hmax);
+	DIE(!ht->buckets, "malloc failed");
+
+	for (uint i = 0; i < hmax; i++) {
+		ht->buckets[i] = ll_create(sizeof(info_t));
+	}
+
+	ht->hash_function = hash_function;
+	ht->compare_function = compare_function;
+	ht->key_val_free_function = key_val_free_function;
+
+	return ht;
+}
+
+int ht_has_key(hashtable_t *ht, void *key)
+{	
+	uint index = ht->hash_function(key) % ht->hmax;
+
+	if (!ht->buckets[index])
+		return 0;
+
+	ll_node_t *find_node = ht->buckets[index]->head;
+
+	while (find_node) {	
+		if (ht->compare_function(key, ((info_t *)find_node->data)->key) == 0)
+			return 1;
+
+		find_node = find_node->next;
+	}
+
+	return 0;
+}
+
+void *ht_get(hashtable_t *ht, void *key)
+{
+	uint index = ht->hash_function(key) % ht->hmax;
+
+	if (!ht->buckets[index])
+		return NULL;
+
+	ll_node_t *find_node = ht->buckets[index]->head;
+
+	while (find_node) {
+		if (ht->compare_function(key, ((info_t *)find_node->data)->key) == 0)
+			return ((info_t *)find_node->data)->value;
+		
+		find_node = find_node->next;
+	}
+
+	return NULL;
+}
+
+void ht_put(hashtable_t *ht, void *key, unsigned int key_size,
+	void *value, unsigned int value_size)
+{
+	uint index = ht->hash_function(key) % ht->hmax;
+
+	linked_list_t *list = ht->buckets[index];
+
+	ll_node_t *curr = list->head;
+
+	int key_exists = 0;
+
+	for (uint i = 0; i < ht->buckets[index]->size; i++) {
+		if (ht->compare_function(key, ((info_t *)curr->data)->key) == 0) {
+			key_exists = 1;
+			break;
+		}
+
+		curr = curr->next;
+	}	
+
+	if (key_exists) {
+		memcpy(((info_t *)curr->data)->value, value, value_size);
+	} else {
+		info_t data;
+
+		data.value = malloc(value_size);
+		DIE(!data.value, "malloc failed");
+
+		data.key = malloc(key_size);
+		DIE(!data.key, "malloc failed");
+
+		memcpy(data.value, value, value_size);
+		memcpy(data.key, key, key_size);
+
+		ll_add_nth_node(list, 0, (const void *)&data);
+
+		ht->size++;
+	}
+}
+
+void ht_remove_entry(hashtable_t *ht, void *key)
+{
+	uint index = ht->hash_function(key) % ht->hmax;
+
+	if (!ht->buckets[index])
+		return;
+
+	linked_list_t *list = ht->buckets[index];
+
+	ll_node_t *curr = list->head;
+
+	int pos = -1;
+
+	for (uint i = 0; i < ht->buckets[index]->size; i++) {
+		if (ht->compare_function(key, ((info_t *)curr->data)->key) == 0) {
+			pos = i;
+			break;
+		}
+
+		curr = curr->next;
+	}
+
+	if (pos == -1)
+		return;
+
+	ll_node_t *removed = ll_remove_nth_node(list, pos);
+
+	key_val_free_function(((info_t *)removed->data));
+
+	free(removed->data);
+	free(removed);
+
+	ht->size--;
+}
+
+void ht_free(hashtable_t *ht)
+{	
+    for (uint i = 0; i < ht->hmax; i++) {
+		if (ht->buckets[i]) {
+			ll_node_t *curr = ht->buckets[i]->head;
+
+			while (curr) {
+				key_val_free_function(curr->data);
+				curr = curr->next;
+			}
+		}
+		ll_free(&ht->buckets[i]);
+	}
+
+	free(ht->buckets);
+	free(ht);
+}
+
+unsigned int ht_get_size(hashtable_t *ht)
+{
+	if (!ht)
+		return 0;
+
+	return ht->size;
+}
+
+unsigned int ht_get_hmax(hashtable_t *ht)
+{
+	if (!ht)
+		return 0;
+
+	return ht->hmax;
+}
---
+++ dictionary.h
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define DIE(assertion, call_description)				\
+	do {								\
+		if (assertion) {					\
+			fprintf(stderr, "(%s, %d): ",			\
+					__FILE__, __LINE__);		\
+			perror(call_description);			\
+			exit(errno);				        \
+		}							\
+	} while (0)
+
+#define MAX_STRING_SIZE	256
+#define HMAX 10
+
+typedef unsigned int uint;
+
+typedef struct ll_node_t
+{
+    void* data;
+    struct ll_node_t* next;
+} ll_node_t;
+
+typedef struct linked_list_t
+{
+    ll_node_t* head;
+    unsigned int data_size;
+    unsigned int size;
+} linked_list_t;
+
+linked_list_t *ll_create(unsigned int data_size);
+
+void ll_add_nth_node(linked_list_t* list, unsigned int n, const void* new_data);
+
+void ll_add_nth_node(linked_list_t* list, unsigned int n, const void* new_data);
+
+ll_node_t *ll_remove_nth_node(linked_list_t* list, unsigned int n);
+
+unsigned int ll_get_size(linked_list_t* list);
+
+void ll_free(linked_list_t** pp_list);
+
+void ll_print_int(linked_list_t* list);
+
+void ll_print_string(linked_list_t* list);
+
+typedef struct info_t {
+	void *key;
+	void *value;
+} info_t;
+
+typedef struct hashtable_t hashtable_t;
+struct hashtable_t {
+	linked_list_t **buckets;
+	unsigned int size;
+	unsigned int hmax;
+	unsigned int (*hash_function)(void*);
+	int (*compare_function)(void*, void*);
+	void (*key_val_free_function)(void*);
+};
+
+int compare_function_ints(void *a, void *b);
+
+int compare_function_strings(void *a, void *b);
+
+unsigned int hash_function_int(void *a);
+
+unsigned int hash_function_string(void *a);
+
+void key_val_free_function(void *data);
+
+hashtable_t *ht_create(unsigned int hmax, unsigned int (*hash_function)(void*),
+		int (*compare_function)(void*, void*),
+		void (*key_val_free_function)(void*));
+
+int ht_has_key(hashtable_t *ht, void *key);
+
+void *ht_get(hashtable_t *ht, void *key);
+
+void ht_put(hashtable_t *ht, void *key, unsigned int key_size,
+	void *value, unsigned int value_size);
+
+void ht_remove_entry(hashtable_t *ht, void *key);
+
+void ht_free(hashtable_t *ht);
+
+unsigned int ht_get_size(hashtable_t *ht);
+
+unsigned int ht_get_hmax(hashtable_t *ht);
